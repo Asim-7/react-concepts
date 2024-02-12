51e077162cad3a25bdc376dae51d149d4d4e7695
Asim <asimpsy11@gmail.com>
2024-02-12 12:03:01 +0100

    Counter page done

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    setCount((prev) => prev + 1); // use prev to add value to last value rather than adding new
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={handleCount}
+      >
+        Click me
+      </button>
+
+      <p>Click is: {count}</p>
+    </>
+  );
 }
 
 export default Counter;
