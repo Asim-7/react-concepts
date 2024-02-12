313189b835d553273985e490b5b03d379da4cdd4
Asim <asimpsy11@gmail.com>
2024-02-12 15:26:27 +0100

    Buttons correctly done

---
+++ src/components/ButtonCustom.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ButtonCustom({ text }) {
+  return (
+    <button
+      type="button"
+      class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+    >
+      {text}
+    </button>
+  );
+}
+
+export default ButtonCustom;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ButtonCustom from "../components/ButtonCustom";
 
 function Home() {
   return (
     <div class="inline-flex rounded-md shadow-sm" role="group">
       <Link to={"/counter"}>
-        <button
-          type="button"
-          class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-        >
-          Counter
-        </button>
+        <ButtonCustom text="Counter" />
       </Link>
 
       <Link to={"/form"}>
-        <button
-          type="button"
-          class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-        >
-          Form
-        </button>
+        <ButtonCustom text="Form" />
       </Link>
 
-      <button
-        type="button"
-        class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-      >
-        Messages
-      </button>
+      <ButtonCustom text="Messages" />
     </div>
   );
 }
