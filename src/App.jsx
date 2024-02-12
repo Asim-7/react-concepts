90b14bb20dd19a10326a276429547bf6e84f829b
Asim <asimpsy11@gmail.com>
2024-02-12 11:55:53 +0100

    Counter page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
+import Counter from "./pages/Counter";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/counter" element={<Counter />} />
       <Route path="/form" element={<Form />} />
     </Routes>
   );
