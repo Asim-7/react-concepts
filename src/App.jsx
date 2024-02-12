c9b9f408958926e5a2e340ce2b3471b3fbe2f9a7
Asim <asimpsy11@gmail.com>
2024-02-12 11:38:26 +0100

    Router added with 2 sample pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/form" element={<Form />} />
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/pages/Form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Form() {
+  return <h1 className="text-3xl font-bold underline">Form page</h1>;
+}
+
+export default Form;
